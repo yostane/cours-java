542d794e113e33a752aa4d77513534138ba84f5b
yostane <1958676+yostane@users.noreply.github.com>
2024-04-17 12:57:50 +0200

    Add stream and collection code

--- material/common/tour/CollectionDemo.java
+++ material/common/tour/CollectionDemo.java
@@ -1,12 +1,57 @@
-///usr/bin/env jbang "$0" "$@" ; exit $?
+///usr/bin/env jbang --enable-preview "$0" "$@" ; exit $?
 
 import static java.lang.System.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class CollectionDemo {
 
   public static void main(String... args) {
+    // Type de base int
+    int i = 99;
+    // Objet Integer
+    Integer j = 10;
+    System.out.println(i);
+    System.out.println("Integer: " + j.compareTo(i) + ", " + j.floatValue());
     int[] numbers = { 1, 20, 30 };
     List<Integer> items = new ArrayList<>();
+    items.add(-3);
+    items.add(11);
+    items.add(22);
+    var otherItems = List.of(-2, 11, 22);
+    for (int k = 0; k < items.size(); k++) {
+      System.out.print(items.get(k) + ", ");
+    }
+    System.out.println();
+    Iterator<Integer> iter = items.iterator();
+    System.out.println(iter.next() + ", " + iter.hasNext());
+    System.out.println(iter.next() + ", " + iter.hasNext());
+    System.out.println(iter.next() + ", " + iter.hasNext());
+    System.out.println();
+    System.out.println("Iter for loop");
+    for (var iter2 = items.iterator(); iter2.hasNext();) {
+      Integer value = iter2.next();
+      System.out.print(value + ", ");
+    }
+    System.out.println();
+    System.out.println("Iter for each");
+    for (Integer item : items) {
+      System.out.print(item + ", ");
+    }
+    System.out.println();
+
+    System.out.println("Map");
+    // 6786L => Litéral de type long (type de base)
+    Map<String, Long> userIds = Map.of("Hugo", 6786L, "Rémy", 343L);
+    System.out.println(userIds.get("Rémy"));
+    Iterator<Entry<String, Long>> iterUserIds = userIds.entrySet().iterator();
+    System.out.println(iterUserIds.next() + ", " + iterUserIds.hasNext());
+    var entry = iterUserIds.next();
+    System.out.println(entry.getKey() + "->" + entry.getValue() + ", " + iterUserIds.hasNext());
+    for (var userIdEntry : userIds.entrySet()) {
+      System.out.print(userIdEntry.getKey() + "->" + userIdEntry.getValue());
+      System.out.print(", ");
+    }
+    System.out.println();
   }
 }
--- material/common/tour/StreamDemo.java
+++ material/common/tour/StreamDemo.java
@@ -1,11 +1,30 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
-
 import static java.lang.System.*;
+import java.util.*;
+import java.util.stream.*;
 
 public class StreamDemo {
 
-    public static void main(String... args) {
-        out.println("Hello World");
-    }
+  public static void main(String... args) {
+    out.println("Hello World");
+    Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 100);
+    var filteredNumbers = numbers.filter((n) -> n % 2 == 0);
+    var doubleNumbers = filteredNumbers.map((n) -> n * 2);
+    var product = doubleNumbers.reduce(Integer::sum);
+    System.out.println(product);
+
+    var otherNumbers = Stream.of(1, 2, 3, 4, 100);
+    var result = otherNumbers
+        .filter((n) -> n % 2 == 0)
+        .map((n) -> n * 2)
+        .reduce(Integer::sum);
+
+    System.out.println(result);
+
+    var pairNumbers = Stream.of(1, 2, 3, 4, 100)
+        .filter((n) -> n % 2 == 0).toList();
+
+    var doublePairs = pairNumbers.stream().map((n) -> n * 2).toList();
+  }
 }
