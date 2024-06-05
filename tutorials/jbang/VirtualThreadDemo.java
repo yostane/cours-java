95a91897577baddeedafa8bc77cb83f7a5d8e11f
yostane <1958676+yostane@users.noreply.github.com>
2024-06-05 16:33:41 +0200

    Add virtual thread demo

---
+++ tutorials/jbang/VirtualThreadDemo.java
@@ -0,0 +1,22 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+import static java.lang.System.*;
+
+public class VirtualThreadDemo {
+
+  public static void main(String... args) throws InterruptedException {
+    Thread virtualThread = Thread.ofVirtual().start(() -> {
+      System.out.println(Thread.currentThread());
+    });
+
+    Thread.ofVirtual().start(() -> {
+      System.out.println(Thread.currentThread());
+    });
+
+    Thread osThread = Thread.ofPlatform().start(() -> {
+      System.out.println(Thread.currentThread());
+    });
+
+    Thread.sleep(1000);
+  }
+}
