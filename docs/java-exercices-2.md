cd89dd26cd3e955ebb4c90ad3b0a480fd975d4ae
yostane <1958676+yostane@users.noreply.github.com>
2024-06-12 12:13:34 +0200

    Add Java exercise 2 and solution

--- docs/java-exercices-2.md
+++ docs/java-exercices-2.md
@@ -14,9 +14,19 @@
     -   Le nombre de valeurs positives et le nombre de valeurs négatives
     -   Le nombre de valeurs paires et le nombre de valeurs impaires
 
+??? "Série 2 exo 1"
+
+    ```java
+    --8<--
+    exo/serie-2/Serie2Ex1.java
+    --8<--
+    ```
+
 ## Exercice 2
 
 Créer une programme Java qui met dans un tableau les 10 premiers nombres premiers posifitfs.
+
+Rappel : un nombre premier est un nombre qui qui est divisible par exactement deux entiers disctints (qui sont 1 et le nombre-même). 0 et 1 ne sont pas des nombres premiers par définition (1 n'est pas divisible par deux nombres distincts).
 
 ## Exercice 3
 
---
+++ material/common/exo/serie-2/Serie2Ex1.java
@@ -0,0 +1,42 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+import java.util.random.RandomGenerator;
+
+public class Serie2Ex1 {
+
+  public static void main(String... args) {
+    int numbers[] = new int[10];
+    RandomGenerator rng = RandomGenerator.getDefault();
+    int sum = 0;
+    int oddCount = 0;
+    int evenCount = 0;
+    // génération et calcul de la somme
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = rng.nextInt(-10, 10);
+      System.out.print(numbers[i] + ", ");
+      sum += numbers[i];
+      if (numbers[i] % 2 == 0) {
+        evenCount += 1;
+      } else {
+        oddCount += 1;
+      }
+    }
+    // min, max, écrat type
+    int min = numbers[0];
+    int max = numbers[0];
+    double standardDeviation = 0; // ecart type
+    for (int i = 0; i < numbers.length; i++) {
+      standardDeviation += Math.pow(numbers[i] - 2, 2);
+      if (min > numbers[i]) {
+        min = numbers[i];
+      } else if (max < numbers[i]) {
+        max = numbers[i];
+      }
+    }
+    standardDeviation = Math.sqrt(standardDeviation / (double) numbers.length);
+    System.out.println("\nResults ->");
+    System.out.println("min: " + min + ", max:" + max);
+    System.out.println("Even count: " + evenCount + ", Odd count:" + oddCount);
+    System.out.println("sum: " + sum + ", Standard deviation:" + standardDeviation);
+  }
+}
