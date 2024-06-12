16b754fb0edaac2b5afa30502dc5fcc2cc01c054
yostane <1958676+yostane@users.noreply.github.com>
2024-06-12 13:00:32 +0200

    Refactor Serie2Ex1.java to improve readability and maintainability

--- material/common/exo/serie-2/Serie2Ex1.java
+++ material/common/exo/serie-2/Serie2Ex1.java
@@ -8,8 +8,8 @@ public class Serie2Ex1 {
     int numbers[] = new int[10];
     RandomGenerator rng = RandomGenerator.getDefault();
     int sum = 0;
-    int oddCount = 0;
-    int evenCount = 0;
+    int oddCount = 0; // odd : impair
+    int evenCount = 0; // even : pair
     // génération et calcul de la somme
     for (int i = 0; i < numbers.length; i++) {
       numbers[i] = rng.nextInt(-10, 10);
@@ -25,8 +25,9 @@ public class Serie2Ex1 {
     int min = numbers[0];
     int max = numbers[0];
     double standardDeviation = 0; // ecart type
+    double average = (double) sum / (double) numbers.length;
     for (int i = 0; i < numbers.length; i++) {
-      standardDeviation += Math.pow(numbers[i] - 2, 2);
+      standardDeviation += Math.pow(numbers[i] - average, 2);
       if (min > numbers[i]) {
         min = numbers[i];
       } else if (max < numbers[i]) {
